119f065ce95af3fc31c43c4adf16dfb6af8e4060
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-09-06 16:07:33 -0700

    notes about maps versus list

--- cart/lib/cart.ex
+++ cart/lib/cart.ex
@@ -14,9 +14,16 @@ defmodule Cart do
   """
   def handle(cart, {:item_added, item}) do
     cart = %{contents: [item | Map.get(cart, :contents, [])]}
+    # Map.update(:bananas, 1, fn(quantity) -> quantity +1 end )
+
   end
   def handle(cart, {:item_removed, item}) do
-    cart =  %{contents: [Map.delete(cart, item)]}
+    cart =  %{contents: []}
+    #List = [1, 2, 3]
+    #List.delete(2)
+    #List.delete_at(1)
+
+    # Map.update(:bananas, 1, fn(quantity) -> quantity +1 end )
   end
   def handle(cart, {:show_contents}) do
     for c <- cart.contents, do: c
